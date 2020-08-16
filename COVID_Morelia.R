b2c638f82c30a457d87bf98efc7873257fd559e5
hsamano <hsamano@lcg.unam.mx>
2020-08-16 11:32:01 -0500

    Averaging added

--- COVID_Morelia.R
+++ COVID_Morelia.R
@@ -10,20 +10,29 @@ Morelia <- read.csv("~/Documents/GitHub/public_scripts/COVID_Morelia.tsv",
 # Correcting the time
 Morelia$Time <- 1:nrow(Morelia)
 Morelia$Date <- as.Date(Morelia$Time, origin = "2020-06-16")
-# 
+# Averaging
+Morelia$Average <- sapply(1:nrow(Morelia), function(x){
+  if(x < 7){
+    return(NA)
+  }
+  mean(Morelia$NewCases[(x-6):x])
+})
 # Plot
-p <- ggplot(Morelia, aes(x = Date)) +
+(p <- ggplot(Morelia, aes(x = Date)) +
   geom_point(aes(y = ActiveCases, colour = "Casos activos")) +
   geom_line(aes(y = ActiveCases, colour = "Casos activos")) +
-  scale_x_date(date_labels = "%b %d", date_breaks = "3 days") +
+  scale_x_date(date_labels = "%b %d", date_breaks = "4 days", limits = c(Morelia$Date[3], max(Morelia$Date))) +
+#  xlim(Morelia$Date[3], max(Morelia$Date)) +
   ylim(-1, max(Morelia$ActiveCases)) +
-  geom_point(aes(y = NewCases, colour = "Nuevos casos")) +
-  geom_line(aes(y = NewCases, colour = "Nuevos casos")) +
-  scale_colour_manual(values = c("tomato", "black")) +
+  geom_bar(stat = "identity", aes(y = NewCases, fill = "Nuevos casos")) +
+  geom_line(aes(y = Average, colour = "7-días promedio")) +
+  scale_colour_manual(values = c("black", "tomato")) +
+  scale_fill_manual(values = c("gray60")) +
   labs(x = "Fecha", y = "Número de casos", colour = "") +
   ggtitle("Casos en Morelia (activos y nuevos)") +
   theme(plot.title = element_text(size=13, face="bold"),
-        legend.position = c(0.1, 0.82),
+        legend.title = element_blank(),
+        legend.position = c(0.36, 0.8),
         legend.background = element_rect(fill = "lightblue"),
         panel.background = element_rect(fill = "lightblue",
                                         colour = "lightblue",
@@ -32,7 +41,7 @@ p <- ggplot(Morelia, aes(x = Date)) +
                                         colour = "white"), 
         panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
                                         colour = "white"),
-        plot.background = element_rect(fill = "lightblue"))
+        plot.background = element_rect(fill = "lightblue")))
 png("COVID_Morelia.png", width = 700, height = 400, units = "px", res = 100)
 print(p)
 dev.off()
