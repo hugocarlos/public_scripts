58c9b936c2166909342bdedb8f31a73dea0640ca
hsamano <hsamano@lcg.unam.mx>
2020-09-26 21:51:33 -0500

    starts in July 1

--- COVID_Morelia.R
+++ COVID_Morelia.R
@@ -29,7 +29,7 @@ Morelia$weekend <- extended_vector[1:nrow(Morelia)]
   geom_bar(stat = "identity", aes(y = NewCases, fill = "Nuevos casos")) +
   geom_line(aes(y = Average, colour = "7-días promedio")) +
   scale_x_date(date_labels = "%b %d", date_breaks = "3 days",
-               limits = c(Morelia$Date[3], max(Morelia$Date) + 1)) +
+               limits = c(Morelia$Date[17], max(Morelia$Date) + 1)) +
   #xlim(Morelia$Date[3], max(Morelia$Date)) +
   ylim(-1, max(Morelia$ActiveCases)) +
   scale_colour_manual(values = c("black", "sienna", "sienna", "orangered")) +
@@ -38,7 +38,7 @@ Morelia$weekend <- extended_vector[1:nrow(Morelia)]
   ggtitle("Casos en Morelia (activos y nuevos)") +
   theme(plot.title = element_text(size=13, face="bold"),
         legend.title = element_blank(),
-        legend.position = c(0.22, 0.8),
+        legend.position = c(0.12, 0.8),
         legend.background = element_rect(fill = "lightblue"),
         panel.background = element_rect(fill = "lightblue",
                                         colour = "lightblue",
