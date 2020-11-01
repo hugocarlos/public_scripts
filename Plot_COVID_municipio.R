b96cb34c69fddf33216ef81466bf0914a379d0a9
hsamano <hsamano@lcg.unam.mx>
2020-11-01 11:53:44 -0600

    3 months plotted

--- Plot_COVID_municipio.R
+++ Plot_COVID_municipio.R
@@ -32,7 +32,7 @@ catalogoEntidades <- read.csv("~/Documents/GitHub/public_scripts/Catalogo_de_ENT
 catalogoMunicipios <- read.csv("~/Documents/GitHub/public_scripts/Catalogo_MUNICIPIOS.tsv", header = TRUE,
                                sep = "\t")
 unzip("~/Documents/Personal/others/datos_abiertos_covid19.zip", exdir = "~/Documents/Personal/others/")
-data <- read.csv("~/Documents/Personal/others/201025COVID19MEXICO.csv",
+data <- read.csv("~/Documents/Personal/others/201031COVID19MEXICO.csv",
                  header = TRUE, quote = "\"", sep = ",")
 
 # Calculating average positivity in the last 7 days
@@ -72,7 +72,7 @@ if(any(!length(un_municipio))){
 }
 
 # Emptying space
-period_plotted <- 50 # Number of bars
+period_plotted <- 90 # Number of bars
 una_fecha <- today - period_plotted + 1
 
 # Filtering by municipality, reducing columns
@@ -126,10 +126,10 @@ df <- data.frame(Dates = as.Date(names(tabla_estimated)), Cases = tabla_estimate
     geom_rect(aes(xmin = df$Dates[nrow(df) - uncertain_period + 1], xmax = df$Dates[nrow(df)] + 0,
                   ymin = 0, ymax = (max(df$Cases) + 2)),
               alpha = 0.3, fill = "tomato") +
-    annotate(geom = "text", x = df$Dates[nrow(df)] - 6, y = (max(df$Cases) - 3),
-             label = "Estos valores", color = "black") +
-    annotate(geom = "text", x = df$Dates[nrow(df)] - 6, y = (max(df$Cases) - 8),
-             label = "pueden aumentar", color = "black") +
+    annotate(geom = "text", x = df$Dates[nrow(df)] - 6, y = (max(df$Cases) - 5),
+             label = "Estos valores", color = "black", size = 3) +
+    annotate(geom = "text", x = df$Dates[nrow(df)] - 6, y = (max(df$Cases) - 10),
+             label = "pueden aumentar", color = "black", size = 3) +
     scale_x_date(date_labels = "%b %d", date_breaks = "2 days") +
     scale_colour_manual(values = c("black", "green4")) +
     labs(x = "Fecha", y = "Número de casos estimados", colour = "") +
@@ -140,7 +140,7 @@ df <- data.frame(Dates = as.Date(names(tabla_estimated)), Cases = tabla_estimate
           legend.title = element_blank(),
 #          legend.background = element_blank(),
           legend.key = element_rect(fill = NULL, color = NULL),
-          plot.title = element_text(size=13, face="bold"),
+          plot.title = element_text(size = 13, face = "bold"),
           axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
           panel.background = element_rect(fill = "lightblue",
                                           colour = "lightblue",
